ef2b27fea03dcf2e3a0564a0f739874a9d8c11ef
alex <alex.m.larson@gmail.com>
2016-04-25 09:56:01 -0700

    inital commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using ProjectAuth.Models;
+
+namespace ProjectAuth.Controllers
+{
+    [Authorize]
+    public class ToDoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ToDoController(
+            UserManager<ApplicationUser> userManager,
+            ApplicationDbContext db
+        )
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            return View(_db.Items.Where(x => x.User.Id == currentUser.Id));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Item item)
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            item.User = currentUser;
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ProjectAuth.Models;
+using Microsoft.Data.Entity;
+
+namespace ProjectAuth.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
